28f36babc041830e7310c4ff0271d01775f0a290
niksingh710 <nik.singh710@gmail.com>
2020-10-16 16:58:57 +0530

    Initial Commit
    Uploading First Time

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# bash_Prompt
+# Bash Prompt
+
 Simple Bash Ps1 Variable Editing to make it look a bit nice.
+
+# Installation
+
+`git clone https://github.com/niksingh710/bash_Prompt.git ~/.bash_customPrompt && echo source ~/.bash_custom_prompt/custom_prompt.sh >> ~/.bashrc && exec bash `
+
+# Images
+
+![Image 1](https://github.com/niksingh710/bash_prompt/blob/main/1.png?raw=true)
+
+![Image 1](https://github.com/niksingh710/bash_prompt/blob/main/2.png?raw=true)
+
+# Requirements
+
+Git needed to be installed and this is tested on Linux with bash shell.
+
+## Ubuntu/Debian based distro
+
+### (Or Distros have apt package manager)
+
+`sudo apt install git`
+
+Other Distro USERS use google 😅.
+
+## Note
+
+The fonts in 'assets/fonts' directory is required to make it work.
+
+## Tip
+
+Try different branches to check new edits.
+
+How to ?
+
+Navigating to directory.
+
+`cd ~/.bash_customPrompt/`
+
+Seeing available branches
+
+`git branch`
+
+To try a branch
+
+`git checkout <branchName> && exec bash`
---
+++ custom_prompt.sh
@@ -0,0 +1,106 @@
+# Custom Bash Prompt for Bash Shell (Just Sourcing PS1 as zsh is not good for my work flow).
+# Created by niksingh710 (https://t.me/niksingh710)
+
+# Variables
+aNormal="\e[0m"
+aBold="\e[1m"
+cMagenta="\e[35m"
+cLGreen="\e[92m"
+cLGrey="\e[37m"
+cDGrey="\e[90m"
+cYellow="\e[33m"
+cLYellow="\e[93m"
+cLRed="\e[91m"
+cLCyan="\e[96m"
+
+cGREETING="$cLGrey $aBold$cLGreen$(whoami)$aNormal$cLYellow $cDGrey $(date +"%A %B %Y %Z")$aNormal\n"
+echo -e "$cGREETING"
+alias clear='clear && echo -e "$cGREETING"'
+
+# Functions
+
+getStatus() {
+	echo "\`
+        if [[ \$? = 0 ]];then
+            echo \[$cLCyan\]🚀;
+        else echo \[$cLRed\]💥;
+    fi
+    \`"
+}
+
+getIcon() {
+	echo "\`
+        if [[ \$(pwd) = "$HOME" ]];then
+            echo ;
+        elif [[ \$(pwd) = "/" ]];then
+         echo ;
+        else echo ;
+    fi
+    \`"
+}
+
+# online Git Copy
+function parse_git_branch() {
+	BRANCH=$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+	if [ ! "${BRANCH}" == "" ]; then
+		STAT=$(parse_git_dirty)
+		echo "[${BRANCH}${STAT}]"
+	else
+		echo ""
+	fi
+}
+
+# get current status of git repo
+function parse_git_dirty() {
+	status=$(git status 2>&1 | tee)
+	dirty=$(
+		echo -n "${status}" 2>/dev/null | grep "modified:" &>/dev/null
+		echo "$?"
+	)
+	untracked=$(
+		echo -n "${status}" 2>/dev/null | grep "Untracked files" &>/dev/null
+		echo "$?"
+	)
+	ahead=$(
+		echo -n "${status}" 2>/dev/null | grep "Your branch is ahead of" &>/dev/null
+		echo "$?"
+	)
+	newfile=$(
+		echo -n "${status}" 2>/dev/null | grep "new file:" &>/dev/null
+		echo "$?"
+	)
+	renamed=$(
+		echo -n "${status}" 2>/dev/null | grep "renamed:" &>/dev/null
+		echo "$?"
+	)
+	deleted=$(
+		echo -n "${status}" 2>/dev/null | grep "deleted:" &>/dev/null
+		echo "$?"
+	)
+	bits=''
+	if [ "${renamed}" == "0" ]; then
+		bits=">${bits}"
+	fi
+	if [ "${ahead}" == "0" ]; then
+		bits="*${bits}"
+	fi
+	if [ "${newfile}" == "0" ]; then
+		bits="+${bits}"
+	fi
+	if [ "${untracked}" == "0" ]; then
+		bits="?${bits}"
+	fi
+	if [ "${deleted}" == "0" ]; then
+		bits="x${bits}"
+	fi
+	if [ "${dirty}" == "0" ]; then
+		bits="!${bits}"
+	fi
+	if [ ! "${bits}" == "" ]; then
+		echo " ${bits}"
+	else
+		echo ""
+	fi
+}
+
+export PS1="$(getStatus) $(getIcon) $aNormal$cLCyan$aBold\W$aNormal $aBold$cLYellow\`parse_git_branch\`$cLCyan $aNormal \@\n>"
